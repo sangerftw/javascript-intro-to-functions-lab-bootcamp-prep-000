52dbefc55dee8f8c1deebfd90fd5a860f8f1bc56
Sara <sangerftw@gmail.com>
2017-12-18 15:41:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
+  
   if (string.toLowerCase()) {string.toLowerCase}
   // if(true) {string.toLowerCase}
   return "I can't hear you!"
