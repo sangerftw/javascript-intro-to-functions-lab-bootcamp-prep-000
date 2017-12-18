5691019c1d4cd06f055e2b3afbabe4c054d7c99b
Sara <sangerftw@gmail.com>
2017-12-18 13:40:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if(true) {string.toLowerCase}
-  return ("I can't hear you!")
+  return "I can't hear you!"
 }
 function sayHiToGrandma(string) {
   if(true) {string.toUpperCase}
-  return ("YES INDEED!")
+  return "YES INDEED!"
 }
 function sayHiToGrandma(string) {
   if(true) {console.log("I love you, Grandma.")}
-  return ("I love you, too.")
+  return "I love you, too."
 }
