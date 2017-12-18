0b749062e4164e852bc418cd2bc2a3ca08719326
Sara <sangerftw@gmail.com>
2017-12-18 15:55:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ function sayHiToGrandma(string) {
   //string = "hi"
   // string.toLowerCase() => 'hi'
   // string.toUpperCase() => 'HI' 
-  if (string.toLowerCase() === string) {return "I can't hear you!"}
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
   // if(true) {string.toLowerCase}
   return "I can't hear you!"
 }
