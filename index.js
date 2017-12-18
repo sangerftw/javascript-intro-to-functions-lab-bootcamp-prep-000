2a8c0968978b56b4c08715131ffe46d8e50ac9f2
Sara <sangerftw@gmail.com>
2017-12-18 13:22:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function sayHiToGrandma(string) {
   return string.toLowerCase("I can't hear you!")
 }
 function sayHiToGrandma(string) {
-  return string.toUpperCase()
+  return string.toUpperCase("YES INDEED!")
 }
